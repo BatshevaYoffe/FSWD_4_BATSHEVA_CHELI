4bdda1079aad87a5b54211869c47d12d7f10553c
BatshevaYoffe <73179986+BatshevaYoffe@users.noreply.github.com>
2023-03-29 19:20:37 +0300

    new react project

---
+++ src/components/Key.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+class Key extends React.Component {
+  render() {return(<div>
+     <h2>Hi, I am a key!</h2>;
+     <button onClick={A}>A</button>
+     <button type=''>B</button>
+    </div>);
+  }
+}
+export default Key;
+
+
---
+++ src/components/Keyboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Key from './Key';
+class Keyboard extends React.Component {
+  render() {
+    return (<div>
+        <h2>Hi, I am a Keyboard!</h2>;
+        <Key />
+        </div>
+    );
+  }
+}
+
+export default Keyboard;
